558bfaf49388a143f8955f9397d912b2279c3a9e
liorg <liorg@jfrog.com>
2019-08-25 22:25:35 +0300

    outh0 - first success

--- src/main/java/com/xmed/MainApplication.java
+++ src/main/java/com/xmed/MainApplication.java
@@ -3,6 +3,8 @@ package com.xmed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -10,6 +12,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 //@EnableJpaRepositories
 //@EnableAsync
 @SpringBootApplication
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:auth0.properties")
+})
 public class MainApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/xmed/resources/NewTestResource.java
+++ src/main/java/com/xmed/resources/NewTestResource.java
@@ -10,8 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.core.MediaType;
 import java.sql.SQLException;
 
@@ -21,15 +25,23 @@ import java.sql.SQLException;
 @Slf4j
 @Api(value = "NewTest")
 @RestController
-@RequestMapping("api/v1/newTest")public class NewTestResource {
+@RequestMapping("api/v1/newTest")
+public class NewTestResource {
 
     @Autowired
     NewTestService newTestService;
 
-    @PostMapping(path = "/create", produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
+    @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
     @ApiOperation(value = "Create New Test")
     @ResponseBody
+    //@RolesAllowed()
     public ResponseEntity CreateNewTest(@RequestBody CreateNewTestRequest newTestRequest) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+        }
+
 
         try {
             NewTestResponse newTestResponse = newTestService.CreateNewTest(newTestRequest);
