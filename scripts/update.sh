246021f7df9cb3c1b3b394b8dccf655aa280b2e0
liorgur <54078679+liorgur@users.noreply.github.com>
2019-09-09 12:21:04 +0300

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker rm -f $(docker ps -aq)
+docker run -d -p 80:8080 liorxmed/xmed:latest
+
 while true; do
     current_digest=$(docker images --digests | grep liorxmed/xmed | grep latest | awk '{print $3}')
     docker pull liorxmed/xmed:latest
