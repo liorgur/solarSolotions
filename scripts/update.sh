4159a0ed3d9d87a48ed55da4b3ac71618518501b
Maor Friedman <maor.friedman@redhat.com>
2019-09-09 12:07:10 +0300

    add update script for development environment

---
+++ scripts/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true; do
+    current_digest=$(docker images --digests | grep liorxmed/xmed | grep latest | awk '{print $3}')
+    docker pull liorxmed/xmed:latest
+    new_digest=$(docker images --digests | grep liorxmed/xmed | grep latest | awk '{print $3}')
+
+    if [ "$current_digest" != "$new_digest"]; then
+        docker rm -f $(docker ps -aq)
+        docker run -d -p 80:8080 liorxmed/xmed:latest
+        docker system prune -f
+    fi
+    sleep 60
+done
