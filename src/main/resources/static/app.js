01c70b9cac5929f566a9e586619b02a9da624f1b
lior.g <lior.g@clewmed.com>
2020-03-04 23:38:37 +0200

    after gauges

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,7 +1,9 @@
-google.charts.load('current', { packages: ['corechart','line','table'] });
+var ip2 = 'localhost:8082'
+var ip = '63.35.216.142'
 
+google.charts.load('current', { packages: ['corechart', 'line', 'table', 'gauge'] });
+var sitesLior;
 function initMap() {
-
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: { lat: 32.1192362, lng: 35.5750295 }
@@ -14,62 +16,59 @@ function initMap() {
     west: 34.80
   };
 
-
   // Display the area between the location southWest and northEast.
   map.fitBounds(bounds);
+  getSitesData().then(data => drawSitesOnMap(map, data)).then(data => drawSiteTable(data));
+}
 
+async function getSitesData() {
+  let response = await fetch('http://' + ip +'/api/v1/Sites/');
+  let data = await response.json()
+  return data.sites;
 
-  var client = new HttpClient();
-  client.get('http://63.35.216.142/api/v1/Sites/', function (response) {
-//  client.get('/api/v1/Sites/', function (response) {
+}
 
-    drewSites(response)
-  });
-
-  function drewSites(response) {
-    var jsonResponse = JSON.parse(response);
-    for (var i = 0; i < jsonResponse.sites.length; ++i) {
+function drawSitesOnMap(map, sitesData){
+ for (var i = 0; i < sitesData.length; ++i) {
       var marker = new google.maps.Marker({
         position: {
-          lat: jsonResponse.sites[i].lat,
-          lng: jsonResponse.sites[i].lon
+          lat: sitesData[i].lat,
+          lng: sitesData[i].lon
         },
         map: map
       });
-      attachMessage(marker, jsonResponse.sites[i]);
+      attachMassage(marker, sitesData[i]);
     }
-  }
-  // Attaches an info window to a marker with the provided message. When the
-  // marker is clicked, the info window will open with the secret message.
-  function attachMessage(marker, message) {
-    var infowindow = new google.maps.InfoWindow({
-      content: (message.name)
-    });
-
-    marker.addListener('click', function () {
-      infowindow.open(marker.get('map'), marker);
-    });
-    marker.addListener('dblclick', handleDB)
-  }
+    return sitesData;
 }
 
-function handleDB(){
-//    console.log(message.name)
-//        console.log(message.ip)
-fetch('http://63.35.216.142/api/v1/data/?ip=2.55.120.218').then(data=>data.json()).then((jsonDataRaw)=>{
-const jsonData = jsonDataRaw.data
-  var data = new google.visualization.DataTable();
+function attachMassage(marker, massage) {
+  var infowindow = new google.maps.InfoWindow({
+    content: (massage.name)
+  });
+
+  marker.addListener('click', function () {
+    infowindow.open(marker.get('map'), marker);
+  });
+  marker.addListener('dblclick', function(){handleDB(massage);});
+}
+
+function handleDB(massage) {
+  fetch('http://' + ip +'/api/v1/data/?ip='+ massage.ip).then(data => data.json()).then((jsonDataRaw) => {
+    const siteData = jsonDataRaw.data
+    var data = new google.visualization.DataTable();
     data.addColumn('datetime', 'time');
     data.addColumn('number', 'tmp');
     data.addColumn('number', 'volt');
     data.addColumn('number', 'light');
     data.addColumn('number', 'humidity');
-    for (var i = 0; i < jsonData.length; i++) {
-          data.addRow([new Date(jsonData[i].time), jsonData[i].tmp, jsonData[i].volt, jsonData[i].light, jsonData[i].humidity]);
+    for (var i = 0; i < siteData.length; i++) {
+      data.addRow([new Date(siteData[i].time), siteData[i].tmp, siteData[i].volt, siteData[i].light, siteData[i].humidity]);
     }
-   drawTable(data)
+    drawDataTable(data)
     drawChart(data)
-})
+    drawMeters(siteData[siteData.length -1])
+  })
 
 }
 
@@ -78,8 +77,8 @@ function drawChart(data) {
   var options = {
     hAxis: {
       title: 'Time',
-         format: 'hh:mm',
-              gridlines: {count: 9}
+      format: 'hh:mm',
+      gridlines: { count: 9 }
     },
     vAxis: {
       title: 'Values'
@@ -90,26 +89,93 @@ function drawChart(data) {
 
   chart.draw(data, options);
 }
-function drawTable(data) {
 
- data.sort({
-      column: 0,
-      desc: true
-    });
+function drawDataTable(data) {
+
+  data.sort({
+    column: 0,
+    desc: true
+  });
   var table = new google.visualization.Table(document.getElementById('table_div'));
 
-   table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+  table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
 }
 
-var HttpClient = function () {
-  this.get = function (aUrl, aCallback) {
-    var anHttpRequest = new XMLHttpRequest();
-    anHttpRequest.onreadystatechange = function () {
-      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-        aCallback(anHttpRequest.responseText);
+function drawSiteTable(sitesData) {
+
+var data = new google.visualization.DataTable();
+    data.addColumn('string', 'name');
+
+    for (var i = 0; i < sitesData.length; i++) {
+      data.addRow([sitesData[i].name, ]);
     }
-    anHttpRequest.open("GET", aUrl, true);
-    // anHttpRequest.setRequestHeader("Access-Control-Allow-Origin", "*");
-    anHttpRequest.send(null);
-  }
+
+  data.sort({
+    column: 0,
+    desc: true
+  });
+  var table = new google.visualization.Table(document.getElementById('site-list'));
+
+  table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
 }
+
+function drawMeters(data) {
+
+    var tmp_data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ["tmp", data["tmp"]]
+        ]);
+
+    var humidity_data = google.visualization.arrayToDataTable([
+              ['Label', 'Value'],
+              ["humidity", data["humidity"]]
+            ]);
+
+    var volt_data = google.visualization.arrayToDataTable([
+               ['Label', 'Value'],
+               ["volt", data["volt"]]
+             ]);
+    var light_data = google.visualization.arrayToDataTable([
+              ['Label', 'Value'],
+              ["light", data["light"]]
+            ]);
+
+
+    var tmp_options = {
+              width: 400, height: 120,
+              redFrom: 90, redTo: 100,
+              yellowFrom:75, yellowTo: 90,
+              minorTicks: 5
+            };
+    var humidity_options = {
+              width: 400, height: 120,
+              redFrom: 90, redTo: 100,
+              yellowFrom:75, yellowTo: 90,
+              minorTicks: 5
+            };
+    var volt_options = {
+              width: 400, height: 120,
+              redFrom: 90, redTo: 100,
+              yellowFrom:75, yellowTo: 90,
+              minorTicks: 5
+            };
+    var light_options = {
+              width: 400, height: 120,
+              redFrom: 90, redTo: 100,
+              yellowFrom:75, yellowTo: 90,
+              minorTicks: 5
+            };
+
+        var tmp_gauge = new google.visualization.Gauge(document.getElementById('tmp_gauge'));
+        var humidity_gauge = new google.visualization.Gauge(document.getElementById('humidity_gauge'));
+        var volt_gauge = new google.visualization.Gauge(document.getElementById('volt_gauge'));
+        var light_gauge = new google.visualization.Gauge(document.getElementById('light_gauge'));
+
+
+        tmp_gauge.draw(tmp_data, tmp_options);
+        humidity_gauge.draw(humidity_data, humidity_options);
+        volt_gauge.draw(volt_data, volt_options);
+        light_gauge.draw(light_data, light_options);
+
+
+      }
--- target/classes/static/app.js
+++ target/classes/static/app.js
@@ -1,7 +1,6 @@
-google.charts.load('current', { packages: ['corechart','line','table'] });
-
+google.charts.load('current', { packages: ['corechart', 'line', 'table', 'gauge'] });
+var sitesLior;
 function initMap() {
-
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: { lat: 32.1192362, lng: 35.5750295 }
@@ -14,72 +13,71 @@ function initMap() {
     west: 34.80
   };
 
-
   // Display the area between the location southWest and northEast.
   map.fitBounds(bounds);
+  getSitesData().then(data => drawSitesOnMap(map, data)).then(data => drawSiteTable(data));
+}
 
+async function getSitesData() {
+  let response = await fetch('http://localhost:8082/api/v1/Sites/');
+  let data = await response.json()
+  return data.sites;
 
-  var client = new HttpClient();
-  client.get('http://63.35.216.142/api/v1/Sites/', function (response) {
-//  client.get('/api/v1/Sites/', function (response) {
+}
 
-    drewSites(response)
-  });
-
-  function drewSites(response) {
-    var jsonResponse = JSON.parse(response);
-    for (var i = 0; i < jsonResponse.sites.length; ++i) {
+function drawSitesOnMap(map, sitesData){
+ for (var i = 0; i < sitesData.length; ++i) {
       var marker = new google.maps.Marker({
         position: {
-          lat: jsonResponse.sites[i].lat,
-          lng: jsonResponse.sites[i].lon
+          lat: sitesData[i].lat,
+          lng: sitesData[i].lon
         },
         map: map
       });
-      attachMessage(marker, jsonResponse.sites[i]);
+      attachMessage(marker, sitesData[i]);
     }
-  }
-  // Attaches an info window to a marker with the provided message. When the
-  // marker is clicked, the info window will open with the secret message.
-  function attachMessage(marker, message) {
-    var infowindow = new google.maps.InfoWindow({
-      content: (message.name)
-    });
-
-    marker.addListener('click', function () {
-      infowindow.open(marker.get('map'), marker);
-    });
-    marker.addListener('dblclick', function () {
-//      location.href = "site_info.html";
-        console.log(message.name)
-        console.log(message.ip)
-//    drawBasic2()
-fetch('http://63.35.216.142/api/v1/data/?ip=2.55.120.218').then(jsonData=>{
-const data = JSON.parse(jsonData)
-   drawTable(data)
-    drawChart(data)
-})
-
-
-    });
-  }
+    return sitesData;
 }
+
+function attachMessage(marker, message) {
+  var infowindow = new google.maps.InfoWindow({
+    content: (message.name)
+  });
+
+  marker.addListener('click', function () {
+    infowindow.open(marker.get('map'), marker);
+  });
+  marker.addListener('dblclick', handleDB)
+}
+
+function handleDB() {
+  //    console.log(message.name)
+  //        console.log(message.ip)
+  fetch('http://63.35.216.142/api/v1/data/?ip=2.55.120.218').then(data => data.json()).then((jsonDataRaw) => {
+    const siteData = jsonDataRaw.data
+    var data = new google.visualization.DataTable();
+    data.addColumn('datetime', 'time');
+    data.addColumn('number', 'tmp');
+    data.addColumn('number', 'volt');
+    data.addColumn('number', 'light');
+    data.addColumn('number', 'humidity');
+    for (var i = 0; i < siteData.length; i++) {
+      data.addRow([new Date(siteData[i].time), siteData[i].tmp, siteData[i].volt, siteData[i].light, siteData[i].humidity]);
+    }
+    drawDataTable(data)
+    drawChart(data)
+    drawMeters(siteData[siteData.length -1])
+  })
+
+}
+
 function drawChart(data) {
-  var data = new google.visualization.DataTable();
-  data.addColumn('datetime', 'time');
-  data.addColumn('number', 'tmp');
-  data.addColumn('number', 'volt');
-  data.addColumn('number', 'light');
-  data.addColumn('number', 'humidity');
-  for (var i = 0; i < jsonData.length; i++) {
-        data.addRow([new Date(jsonData[i].time), jsonData[i].tmp, jsonData[i].volt, jsonData[i].light, jsonData[i].humidity]);
-  }
 
   var options = {
     hAxis: {
       title: 'Time',
-         format: 'hh:mm',
-              gridlines: {count: 9}
+      format: 'hh:mm',
+      gridlines: { count: 9 }
     },
     vAxis: {
       title: 'Values'
@@ -90,26 +88,95 @@ function drawChart(data) {
 
   chart.draw(data, options);
 }
-function drawTable(data) {
 
- data.sort({
-      column: 0,
-      desc: true
-    });
+function drawDataTable(data) {
+
+  data.sort({
+    column: 0,
+    desc: true
+  });
   var table = new google.visualization.Table(document.getElementById('table_div'));
 
-   table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+  table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
 }
 
-var HttpClient = function () {
-  this.get = function (aUrl, aCallback) {
-    var anHttpRequest = new XMLHttpRequest();
-    anHttpRequest.onreadystatechange = function () {
-      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-        aCallback(anHttpRequest.responseText);
+function drawSiteTable(sitesData) {
+
+var data = new google.visualization.DataTable();
+    data.addColumn('string', 'name');
+//        data.addColumn('string', 'name2');
+
+    for (var i = 0; i < sitesData.length; i++) {
+      data.addRow([sitesData[i].name, ]);
+
     }
-    anHttpRequest.open("GET", aUrl, true);
-    // anHttpRequest.setRequestHeader("Access-Control-Allow-Origin", "*");
-    anHttpRequest.send(null);
-  }
+
+  data.sort({
+    column: 0,
+    desc: true
+  });
+  var table = new google.visualization.Table(document.getElementById('site-list'));
+
+  table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
 }
+
+function drawMeters(data) {
+
+    var tmp = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ["tmp", data["tmp"]]
+        ]);
+
+    var humidity = google.visualization.arrayToDataTable([
+              ['Label', 'Value'],
+              ["humidity", data["humidity"]]
+            ]);
+
+    var volt = google.visualization.arrayToDataTable([
+               ['Label', 'Value'],
+               ["volt", data["volt"]]
+             ]);
+    var light = google.visualization.arrayToDataTable([
+              ['Label', 'Value'],
+              ["light", data["light"]]
+            ]);
+
+
+    var tmp_options = {
+              width: 400, height: 120,
+              redFrom: 90, redTo: 100,
+              yellowFrom:75, yellowTo: 90,
+              minorTicks: 5
+            };
+    var humidity_options = {
+              width: 400, height: 120,
+              redFrom: 90, redTo: 100,
+              yellowFrom:75, yellowTo: 90,
+              minorTicks: 5
+            };
+    var volt_options = {
+              width: 400, height: 120,
+              redFrom: 90, redTo: 100,
+              yellowFrom:75, yellowTo: 90,
+              minorTicks: 5
+            };
+    var light_options = {
+              width: 400, height: 120,
+              redFrom: 90, redTo: 100,
+              yellowFrom:75, yellowTo: 90,
+              minorTicks: 5
+            };
+
+        var tmp_gauge = new google.visualization.Gauge(document.getElementById('tmp_gauge'));
+        var humidity_gauge = new google.visualization.Gauge(document.getElementById('humidity_gauge'));
+        var volt_gauge = new google.visualization.Gauge(document.getElementById('volt_gauge'));
+        var light_gauge = new google.visualization.Gauge(document.getElementById('light_gauge'));
+
+
+        chart.draw(tmp_gauge, tmp_options);
+        chart.draw(humidity_gauge, humidity_options);
+        chart.draw(volt_gauge, volt_options);
+        chart.draw(light_gauge, light_options);
+
+
+      }
